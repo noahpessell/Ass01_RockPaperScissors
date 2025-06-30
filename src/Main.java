d91f79ac124efb27d6f660d1648134a13bbbeab2
noahpessell <nwpessell@mail.com>
2025-06-30 17:03:34 -0400

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Scanner in = new Scanner(System.in);
+    String playA = "";
+    String playB = "";
+    String trash = "";
+    boolean done = false;
+    do {
+        if(playA.equalsIgnoreCase("R"))
+        {
+            if(playB.equalsIgnoreCase("R"))
+            {
+                System.out.println("Its a tie R X R");
+            }
+            else if(playB.equalsIgnoreCase("P"))
+            {
+                System.out.println("Paper covers Rock Player B wins");
+            }
+            else //playB is Scissors
+            {
+                System.out.println("Rock breaks Scissors Player A wins");
+            }
+        }
+        else if(playA.equalsIgnoreCase("P"))
+        {
+
+        }
+        else //playA is Scissors
+        {
+
+        }
+    }while(!done);
+
     }
-}
+    }
+
